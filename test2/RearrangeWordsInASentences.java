4b9bb6146d0cc82c0300baeccaf8f5e7c936c6a5
Poovarasan M <100762713+PoovarasanMoorthy29@users.noreply.github.com>
2026-02-16 22:11:53 +0530

    Refactor rearrangement logic for clarity and efficiency
    - push an optimized solution

--- test2/RearrangeWordsInASentences.java
+++ test2/RearrangeWordsInASentences.java
@@ -8,49 +8,14 @@ import java.util.Arrays;
 public class RearrangeWordsInASentences {
 
     private String getRearrangedStr(String s){
-        String[] str=s.split(" "); // ["Leetcode","makes","me","happy"]
-        int [] strSize=new int[str.length];
-        System.out.println("StringArr: "+Arrays.toString(str));
-
-
-        for(int i=0;i<str.length;i++){
-
-            strSize[i]=str[i].length();
+       String [] str=s.toLowerCase().split(" ");
+        Arrays.sort(str,(a,b)-> a.length()-b.length());
+        StringBuilder sb=new StringBuilder();
+        sb.append(str[0].substring(0,1).toUpperCase()+str[0].substring(1));
+        for(int i=1;i<str.length;i++){
+            sb.append(" "+str[i].toLowerCase());
         }
-        System.out.println("StrSize:" +Arrays.toString(strSize));
-        Arrays.sort(strSize); //[2,5,5,8].
-        System.out.println("SortedArrSize: "+Arrays.toString(strSize));
-        ArrayList<String> list=new ArrayList<>();
-
-        for(int j=0;j<str.length;j++){
-            for(int k=0;k<str.length;k++){
-                if(strSize[j]==str[k].length()){
-                    if(!list.contains(str[k])){
-                        list.add(str[k]);
-                    }
-
-                }
-            }
-
-        }
-        String rearrangedString="";
-        for(int z=0;z<list.size();z++){
-            if(z==0){
-                String strs=list.get(z);
-                char c[]=strs.toCharArray();
-                c[0] =Character.toUpperCase(c[0]);
-
-                rearrangedString+=new String(c)+" ";
-            }
-
-            else if(z!=list.size()-1){
-                rearrangedString+=list.get(z)+" ";
-            }
-            else{
-                rearrangedString+=list.get(z);
-            }
-        }
-     return rearrangedString;
+        return sb.toString().trim();
     }
     public static void main(String[] args) {
         RearrangeWordsInASentences obj=new RearrangeWordsInASentences();
@@ -59,3 +24,4 @@ public class RearrangeWordsInASentences {
     }
 
 }
+
